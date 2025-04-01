fbffc4aebf35a4518fb32c4b05a264e29786042f
Jorge Hidalgo <deors@hotmail.com>
2025-04-01 18:14:27 +0200

    add granite 3.1 dense examples

---
+++ src/main/java/deors/training/langchain4j/OllamaGranite31DenseAskAboutCode.java
@@ -0,0 +1,9 @@
+package deors.training.langchain4j;
+
+public class OllamaGranite31DenseAskAboutCode {
+
+    void main() {
+        // granite3.1-dense:8b model running locally with Ollama
+        OllamaBaseAskAboutCode.askAboutCode("http://localhost:11434", "granite3.1-dense:8b");
+    }
+}
---
+++ src/main/java/deors/training/langchain4j/OllamaGranite31DenseSimpleContext.java
@@ -0,0 +1,9 @@
+package deors.training.langchain4j;
+
+public class OllamaGranite31DenseSimpleContext {
+
+    void main() {
+        // granite3.1-dense:8b model running locally with Ollama
+        OllamaBaseSimpleContext.simpleContext("http://localhost:11434", "granite3.1-dense:8b");
+    }
+}

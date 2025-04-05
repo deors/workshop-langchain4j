ab5b5ea87b54e9172b3e6e96f8c14df2c81706e7
Jorge Hidalgo <deors@hotmail.com>
2025-04-05 11:14:40 +0200

    add example with granite-code

---
+++ src/main/java/deors/training/langchain4j/OllamaCodeGraniteCodeAssist.java
@@ -0,0 +1,39 @@
+package deors.training.langchain4j;
+
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.ollama.OllamaChatModel;
+
+public class OllamaCodeGraniteCodeAssist {
+
+    void main() {
+        // granite-code:8b model running locally with Ollama
+        ChatLanguageModel model = OllamaChatModel.builder()
+            .baseUrl("http://localhost:11434")
+            .modelName("granite-code:8b")
+            .temperature(0.0)
+            .build();
+
+        // simulating a code completion request
+        String message = """
+            <fim_prefix>import java.time.LocalDate;
+            import java.time.Period;
+
+            class AgeCalculator {
+                static int calculateAge(LocalDate birthDate, LocalDate currentDate) {
+                    if (birthDate != null && currentDate != null) {
+                        <fim_suffix>
+                    } else {
+                        return 0;
+                    }
+                }
+            }<fim_middle>
+        """;
+        System.out.println("\n>>>\n" + message);
+
+        // code expected could be:
+        // return Period.between(birthDate, currentDate).getYears();
+
+        String answer = model.generate(message);
+        System.out.println(answer);
+    }
+}
